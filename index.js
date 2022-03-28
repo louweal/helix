4f4664a3c37a28b829ee21b3ed38d64c8ec368c7
louweal <anneloeslouwe@live.nl>
2022-03-28 19:09:44 +0200

    testing hedera stuff

---
+++ index.js
@@ -0,0 +1,20 @@
+console.clear();
+require("dotenv").config();
+const {
+  Client,
+  AccountId,
+  PrivateKey,
+  TokenCreateTransaction,
+  FileCreateTransaction,
+  FileAppendTransaction,
+  ContractCreateTransaction,
+  ContractFunctionParameters,
+  TokenUpdateTransaction,
+  ContractExecuteTransaction,
+  TokenInfoQuery,
+  AccountBalanceQuery,
+  Hbar,
+} = require("@hashgraph/sdk");
+const fs = require("fs");
+
+console.log("hello");
