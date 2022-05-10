ea279d1b9e85848027a2e4a11db80cdffce33558
Anneloes <anneloes@commandos.nl>
2022-05-10 17:30:53 +0200

    ...

--- hedera/LookupContract.sol
+++ hedera/LookupContract.sol
@@ -12,23 +12,33 @@ contract LookupContract {
     }
 
     // not possible to get the return value with the HSCS? :(
-    function getAllContracts(address _accountId) public view returns (address[] memory) {
+    function getAllContracts(address _accountId)
+        public
+        view
+        returns (address[] memory)
+    {
         return store[_accountId];
     }
 
-    function getContract(address _accountId, uint32 i) public view returns (address) {
+    function getContract(address _accountId, uint32 i)
+        public
+        view
+        returns (address)
+    {
         return store[_accountId][i];
     }
 
-    function addContract(address _accountId, address _contractId) public onlyOperator() {
+    function addContract(address _accountId, address _contractId)
+        public
+        onlyOperator
+    {
         store[_accountId].push(payable(_contractId));
     }
 
     // remove item by index
-    function deleteContract(address _accountId, uint32 i) public onlyOperator() {
+    function deleteContract(address _accountId, uint32 i) public onlyOperator {
         delete store[_accountId][i];
     }
-
 
     // modifiers
 
@@ -36,7 +46,7 @@ contract LookupContract {
     error OnlyOperator();
 
     modifier onlyOperator() {
-        if(msg.sender != operator) {
+        if (msg.sender != operator) {
             revert OnlyOperator();
         }
         _;
